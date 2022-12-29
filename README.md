3d5386561f15e1b3942b272eeb7c2c0f506b2b58
Amadeo Bon <amadeosph@outlook.com>
2022-12-29 02:02:04 -0300

    10 métodos JS

---
+++ README.md
@@ -0,0 +1,6 @@
+##
+Exercício
+
+Criar uma página HTML e com Javascript inserir 10 formas diferentes do texto "Hello World".Cada forma deve variar utilizando métodos nativos ou não do Javascript.Usar CSS para criar formas diferentes também é uma possibilidade, mas esgote as possibilidades com JS.
+
+##
---
+++ src/scripts.js
@@ -0,0 +1,79 @@
+window.addEventListener("load", function() {
+  alert("Hello World......Oi, professor(a)!");
+});
+function inserirTexto(elemento, texto) {
+  // Método 1: innerHTML
+  elemento.innerHTML = texto;
+  elemento.style.font = "25px bold";
+
+
+  // Método 2: textContent
+  elemento.textContent = texto;
+
+  // Método 3: appendChild
+  const novoElemento = document.createElement("p");
+  novoElemento.textContent = texto;
+  elemento.appendChild(novoElemento);
+
+  // Método 4: createTextNode
+  const novoNovoTexto = document.createTextNode(texto);
+  elemento.appendChild(novoNovoTexto);
+
+  // Método 5: insertAdjacentHTML
+  elemento.insertAdjacentHTML("beforeend", texto);
+
+  // Método 6: insertAdjacentText
+  elemento.insertAdjacentText("beforeend", texto);
+
+  // Método 7: setAttribute
+  elemento.setAttribute("data-texto", texto);
+
+  // Método 8: style.color
+  elemento.style.color = "red";
+  elemento.textContent = texto;
+
+  // Método 9: classList.add
+  elemento.classList.add("texto-destaque");
+  elemento.textContent = texto;
+
+
+  // Método 10: insertBefore
+  const novoElemento2 = document.createElement("p");
+  novoElemento2.textContent = texto;
+  elemento.parentNode.insertBefore(novoElemento2, elemento);
+}
+
+// Chame a função de inserção de texto em diferentes elementos da página
+inserirTexto(document.getElementById("elemento1"), "Hello World!");
+inserirTexto(document.getElementsByTagName("p")[0], "Hello World!");
+inserirTexto(document.querySelector("#elemento2"), "Hello World!");
+inserirTexto(document.getElementsByTagName("span")[0], "Hello World!");
+inserirTexto(document.getElementsByTagName("li")[0], "Hello World!");
+inserirTexto(document.getElementsByTagName("li")[1], "Hello World!");
+inserirTexto(document.getElementsByTagName("li")[2], "Hello World!");
+
+function inserirTextoMaquinaEscrever(elemento, texto, intervalo) {
+  // Adicione uma animação de transição no estilo CSS
+  elemento.style.transition = "all 0.1s";
+
+  // Use uma função recursiva para inserir cada caractere do texto com um atraso
+  // especificado pelo intervalo
+  (function escrever(i) {
+    if (i < texto.length) {
+      elemento.innerHTML += texto.charAt(i);
+      setTimeout(function() {
+        escrever(i + 1);
+      }, intervalo);
+    }
+  })(0);
+}
+
+// Chame a função passando o elemento, o texto e o intervalo em milissegundos
+inserirTextoMaquinaEscrever(document.getElementById("elemento1"), "Hello World!", 100);
+
+
+
+
+
+
+
